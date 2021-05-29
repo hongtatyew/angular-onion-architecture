295b2a7a133e66a2f4e3072e78231892f6dde860
HongTat <ht_yew@hotmail.com>
2021-05-29 17:40:56 +0800

    initial commit

---
+++ apps/my-hero-academia/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-hero-academia-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'my-hero-academia';
+}
